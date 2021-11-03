98d3b91d74dbbb655c1adbbfd401f826508d71e0
marcos <marcosmourabro@gmail.com>
2021-11-03 16:24:53 -0300

    change deploy again

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,11 @@
 set -e
 
 $(aws ecr get-login --no-include-email)
-export ENVIROMENT_DEPLOY=$1
-export ECR_IMAGE_URL=868884350453.dkr.ecr.us-east-1.amazonaws.com/team-data-capture/the-watcher-bot:$1
+#export ENVIROMENT_DEPLOY=$1
+#export ECR_IMAGE_URL=868884350453.dkr.ecr.us-east-1.amazonaws.com/team-data-capture/the-watcher-bot:$1
 
-docker-compose image = $ECR_IMAGE_URL
-docker-compose build
-docker-compose push
+docker-compose -f build.yml build
+docker-compose -f build.yml push
 
-docker push $ECR_IMAGE_URL
-docker rmi $(docker image ls -q $ECR_IMAGE_URL)
+#docker push $ECR_IMAGE_URL
+#docker rmi $(docker image ls -q $ECR_IMAGE_URL)
