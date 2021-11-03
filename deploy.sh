0dfef71f29b34d934b9071c64df6428336f4042e
marcos <marcosmourabro@gmail.com>
2021-11-03 15:25:16 -0300

    deploy config

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+$(aws ecr get-login --no-include-email)
+export ENVIROMENT_DEPLOY=$1
+export ECR_IMAGE_URL=868884350453.dkr.ecr.us-east-1.amazonaws.com/team-data-capture/the-watcher-bot:$1
+
+npx json -I -f docker-compose.yml -e "this.image"="'$ECR_IMAGE_URL'"
+
+docker-compose -f build.yml build
+docker-compose -f build.yml push
+
+docker push $ECR_IMAGE_URL
+docker rmi $(docker image ls -q $ECR_IMAGE_URL)
