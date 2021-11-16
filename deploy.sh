28fdc05fec2f543b00d829cde051e973e877bed3
marcos <marcosmourabro@gmail.com>
2021-11-16 20:39:34 -0300

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ $(aws ecr get-login --no-include-email)
 docker-compose -f build.yml build
 docker-compose -f build.yml push
 
-#docker push $ECR_IMAGE_URL
-#docker rmi $(docker image ls -q $ECR_IMAGE_URL)
+docker push 868884350453.dkr.ecr.us-east-1.amazonaws.com/team-data-capture/the-watcher-bot:latest
+docker rmi $(docker image ls -q 868884350453.dkr.ecr.us-east-1.amazonaws.com/team-data-capture/the-watcher-bot:latest)
